0398230258f192d3d16ea3b13a3c8f33882f4992
pish11010 <pish11010@gmail.com>
2020-02-17 12:37:25 +0900

    simple example

--- app/src/main/java/com/heunghan/ktorsmaple/MainActivity.kt
+++ app/src/main/java/com/heunghan/ktorsmaple/MainActivity.kt
@@ -1,12 +1,33 @@
 package com.heunghan.ktorsmaple
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import io.ktor.client.HttpClient
+import io.ktor.client.request.get
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        request()
+    }
+
+    private fun request() {
+        CoroutineScope(Dispatchers.IO).launch {
+            get()
+        }
+    }
+
+    private suspend fun get() {
+        val client = HttpClient()
+
+        val result = client.get<String>("https://api.github.com")
+
+        client.close()
     }
 }
