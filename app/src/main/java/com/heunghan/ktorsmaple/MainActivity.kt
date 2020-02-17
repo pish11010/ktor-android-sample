0b37d6046330097aa5d11f9cbcb2acb39313a1ee
pish11010 <pish11010@gmail.com>
2020-02-17 13:17:54 +0900

    add GsonSerializer()

--- app/src/main/java/com/heunghan/ktorsmaple/MainActivity.kt
+++ app/src/main/java/com/heunghan/ktorsmaple/MainActivity.kt
@@ -2,7 +2,10 @@ package com.heunghan.ktorsmaple
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.gson.annotations.SerializedName
 import io.ktor.client.HttpClient
+import io.ktor.client.features.json.GsonSerializer
+import io.ktor.client.features.json.JsonFeature
 import io.ktor.client.request.get
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -24,10 +27,16 @@ class MainActivity : AppCompatActivity() {
     }
 
     private suspend fun get() {
-        val client = HttpClient()
-
-        val result = client.get<String>("https://api.github.com")
-
+        val client = HttpClient() {
+            install(JsonFeature) {
+                serializer = GsonSerializer()
+            }
+        }
+        val result: Github = client.get("https://api.github.com")
         client.close()
     }
+
+    private data class Github(
+        @SerializedName("current_user_url") val currentUserUrl: String
+    )
 }
